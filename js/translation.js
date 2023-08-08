9bdfa3a5e8f9b707dbd9e69479822576ba8d69b0
hazem <haazm.994@gmail.com>
2023-08-09 00:56:13 +0300

    translated

---
+++ js/translation.js
@@ -0,0 +1,255 @@
+const translations = {
+  en: {
+    home: "Home",
+    about: "About",
+    game : "Game",
+    NFT : 'NFT',
+    Token : 'Token',
+    Roadmap : 'Roadmap',
+    contact: "Contact-us",
+    selectLanguage: "Chinese",
+    twitter :"Twitter",
+    smurfs: "SMURFS",
+    token : "TOKEN",
+    benefit : "Benefit from smurfs by collecting blueberries using decentralized application.",
+    whitepaper : "Whitepaper",
+    presale : "Pre-Sale",
+    tgch : "Telegram Channel",
+    tgen : "Telegram English",
+    tgchi : "Telegram Chinese",
+    ourMission : "OUR MISSION",
+    aboutST : "The project smurfs token started in second quarter of 2023 , the project is made of multiple aspects. first, to create a crypto token by the name smurfs token. second,to create an API that will increase functionality for developers,so they can integrate it with their systems and make the payment process simple. third,to develop a B2E game by the name blueberry through collecting blueberries.fourthli, NFT market to buy and sell smurfs nft and develop it.",
+    sam: "Sam Freude",
+    saminfo: "CEO 4 years of experience in developing and managing crypto currency projects.",
+    sarah : "Sarah",
+    sarahinfo : "marketing director with experience in marketing.",
+    blueberry : "BLUEBERRY GAME",
+    nftb2e : "NFT smurfs was submitted inside the game with smurfs personalities and different abilities. who own any of it will get daily return from blueberries according to smurf ability and specifications and it will be source of earning money. who owns the special character papa smurf will get the highest daily return and he has the right to vote on the project path.",
+    readmore : "it's limited hours work and it increases with developing nft. we have six smurfs:",
+    papasm:"1- papa smurf the special character.",
+    smurfette :"2- smurfette the most skilled one in collecting blueberries.",
+    qsm :"3- quick smurf is the fastest between the others.",
+    stsm:"4- strong smurf has stamina.",
+    jksm :"5- smurf joking loves to have fun with smurfs.",
+    clsm:"6- clumsy smurf tripping over anything.",
+    readmoreBtn : "Read more..",
+    appstore : "App Store",
+    playstore : "Play Store",
+    smmarket :"Smurfs market NFT",
+    smmarketin : "A market to buy and sell nft smurfs, with developing to increase the hours of mining , it will be launched after finish vesting.",
+    limited : "NFT IS A LIMITED NUMBER",
+    papasm1:"papa smurf the special character.",
+    smurfette2 :"smurfette the most skilled one in collecting blueberries.",
+    qsm3 :"quick smurf is the fastest between the others.",
+    stsm4:"strong smurf has stamina.",
+    jksm5 :"smurf joking loves to have fun with smurfs.",
+    clsm6:"clumsy smurf tripping over anything.",
+    tokenmic:"TOKENOMICS",
+    renounce :"Renounce Ownership after Launch",
+    max :"Max supply 1billion.",
+    m200 :"tokens game 200M for mining not for supply.",
+    plat:"tokens platforms 100M not for supply.",
+    t10:"tokens marketing 100M vesting 10% every 10 days to ensure strong marketing word and deed for hundred days",
+    maxs:"Max supply ",
+    totals:"Total supply ",
+    pres:"Presale ",
+    liq:"Liquidity ",
+    mar:"Marketing ",
+    cex:"Cex ",
+    gamenft:"Game ",
+    team:"Team ",
+    roadm:"Roadmap",
+    Q1:"Q2 2023",
+    Buil:"Built our team",
+    Weblaunch:"Website launch",
+    whitepaperR:"Whitepaper release",
+    Q2:"Q3 2023",
+    Gov:"Governance development",
+    ido:"IDO",
+    marketing:"Marketing campaign",
+    CMC:"CMC",
+    CG:"CG",
+    Q3:"Q4 2023",
+    gameR:"Game release",
+    nftR:"Releasing NFT market smurfs",
+    mark10:"Marketing every 10 days for 100 days.",
+    Q4:"Q1 2024",
+    apiLaunch:"API launch",
+    listing:"Listing major cex",
+    Q5:"Q2 2024",
+    roadmapv2:"Road map V2",
+    partners:"Partners",
+    needh:"Need Help ? ",
+    contactUs:"Contact Us",
+    email:"support@smurfsst.com",
+    sendM:"Send Message",
+    cu:"CONTACT US",
+    Email:"Email : ",
+    by:"By",
+    copyr:"Copyright © 2023. All Rights Reserved",
+    st:"SMURFS TOKEN"
+
+
+
+    
+
+
+
+
+  },
+  ch: {
+    home: "家",
+    about: "关于",
+    game : "游戏",
+    NFT : 'NFT',
+    Token:'代币',
+    Roadmap : '路线图',
+    contact: "联系我们",
+    selectLanguage: "English",
+    twitter :"推特",
+    smurfs :"蓝精灵",
+    token :"代币",
+    benefit : "通过使用去中心化应用程序收集蓝莓来从蓝精灵中受益。",
+    whitepaper : "白皮书",
+    presale : "预售",
+    tgch : "电报频道",
+    tgen : "电报英语",
+    tgchi : "电报中文",
+    ourMission : "我们的任务",
+    aboutST :"smurfs token 项目于 2023 年第二季度启动，该项目由多个方面组成。 首先，创建一个名为 smurfs token 的加密代币。 其次，创建一个API来增加开发人员的功能，以便他们可以将其与他们的系统集成并使支付过程变得简单。 第三，通过收集蓝莓，开发一款名为blueberry的B2E游戏。第四，NFT市场买卖蓝精灵nft并开发。",
+    sam :"萨姆·弗洛伊德",
+    saminfo :"CEO 拥有 4 年开发和管理加密货币项目的经验。",
+    sarahinfo:"营销总监，具有营销经验。",
+    sarah :"莎拉",
+    blueberry :"蓝莓游戏",
+    nftb2e : "NFT 蓝精灵在游戏内提交，具有蓝精灵的个性和不同的能力。 拥有其中任何一个的人都会根据蓝精灵的能力和规格每天获得蓝莓的回报，这将是赚钱的来源。 谁拥有特殊角色蓝爸爸，谁将获得最高的每日回报，并拥有对项目路径的投票权。",
+    readmore :"它的工作时间有限，并且随着 nft 的发展而增加。 我们有六个蓝精灵：",
+    papasm:"1- 蓝爸爸这个特殊角色。",
+    smurfette :"2- 蓝精灵是采集蓝莓最熟练的一个。",
+    qsm :"3-快速蓝精灵是其他蓝精灵中最快的。",
+    stsm:"4-强大的蓝精灵有耐力。",
+    jksm :"5- 蓝精灵开玩笑喜欢和蓝精灵一起玩耍。",
+    clsm:"6- 笨手笨脚的蓝精灵被任何东西绊倒。",
+    readmoreBtn : "阅读更多..",
+    appstore :"应用商店",
+    playstore : "应用商店",
+    smmarket : "蓝精灵市场 NFT",
+    smmarketin :"一个买卖nft smurfs的市场，随着挖矿时间的增加而发展，将在完成归属后推出。",
+    limited : "NFT 数量有限",
+    papasm1:"蓝爸爸这个特殊角色。",
+    smurfette2 :"蓝精灵是采集蓝莓最熟练的一个。",
+    qsm3 :"快速蓝精灵是其他蓝精灵中最快的。",
+    stsm4:"强大的蓝精灵有耐力。",
+    jksm5 :"蓝精灵开玩笑喜欢和蓝精灵一起玩耍。",
+    clsm6:"笨手笨脚的蓝精灵被任何东西绊倒。",
+    tokenmic:"代币经济学",
+    renounce :"发布后放弃所有权",
+    max :"最大供应量10亿。",
+    m200 :"游戏代币200M用于挖矿，不用于供应。",
+    plat:"代币平台100M不供供应。",
+    t10:"代币营销 100M 每 10 天归属 10%，确保百日强有力的营销言行",
+    maxs:"最大供应量",
+    totals:"总供应量",
+    pres:"预售",
+    liq:"流动性",
+    mar:"营销",
+    cex:"塞克斯",
+    gamenft:"游戏",
+    team:"团队",
+    roadm:"路线图",
+    Q1:"Q2 2023",
+    Buil:"建立了我们的团队",
+    Weblaunch:"网站上线",
+    whitepaperR:"白皮书发布",
+    Q2:"Q3 2023",
+    Gov:"治理发展",
+    ido:"我愿意",
+    marketing:"营销活动",
+    CMC:"羧甲基纤维素钠",
+    CG:"CG",
+    Q3:"Q4 2023",
+    gameR:"游戏发布",
+    nftR:"释放NFT市场蓝精灵",
+    mark10:"每 10 天进行一次营销，持续 100 天。",
+    Q4:"Q1 2024",
+    apiLaunch:"API发布",
+    listing:"列出主要 cex",
+    Q5:"Q2 2024",
+    roadmapv2:"路线图V2",
+    partners:"伙伴",
+    needh:"需要帮忙 ？",
+    contactUs:"联系我们",
+    email:"support@smurfsst.com",
+    sendM:"发信息",
+    cu:"联系我们",
+    Email:"电子邮件 : ",
+    by:"经过",
+    copyr:"版权所有 © 2023。保留所有权利",
+    by:"",
+    st:"蓝精灵令牌"
+
+
+
+
+
+
+
+
+
+  },
+};
+
+const languageButton = document.getElementById("languageButton");
+languageButton.addEventListener("click", () => {
+  const currentLanguage = localStorage.getItem("lang") || "en";
+  const newLanguage = currentLanguage === "en" ? "ch" : "en";
+  setLanguage(newLanguage);
+  localStorage.setItem("lang", newLanguage);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en";
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+};
+
+
+
+
+
+
+
+function changePlaceholderLanguage(currentLanguage) {
+  var placeholderName = document.getElementById("name");
+  var placeholderEmail = document.getElementById("email");
+  var placeholderSubject = document.getElementById("subject");
+  var placeholderMessage = document.getElementById("message");
+
+
+
+  if (currentLanguage === 'en') {
+    placeholderName.placeholder = "Your Name";
+    placeholderEmail.placeholder = "Your Email";
+    placeholderSubject.placeholder = "Subject";
+    placeholderMessage.placeholder = "Message";
+  } else {
+    placeholderName.placeholder = "你的名字";
+    placeholderEmail.placeholder = "你的邮件";
+    placeholderSubject.placeholder = "主题";
+    placeholderMessage.placeholder = "信息";
+  }
+
+}
+languageButton.addEventListener("change", changePlaceholderLanguage);
+
+window.addEventListener('DOMContentLoaded', changePlaceholderLanguage);
+
+
+
