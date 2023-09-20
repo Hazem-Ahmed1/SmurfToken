502ad6f99d11bb63332bd371e9308443c6faeef5
hazem <haazm.994@gmail.com>
2023-09-20 23:12:24 +0300

    mo

---
+++ Smurf token/js/translation.js
@@ -0,0 +1,282 @@
+const translations = {
+  en: {
+    home: "Home",
+    about: "About",
+    game : "Game",
+    NFT : 'NFT',
+    Token : 'Token',
+    Roadmap : 'Roadmap',
+    contact: "Contact-us",
+    selectLanguage: "Chinese",
+    twitter :"Twitter",
+    smurfs: "SMURFS",
+    token : "TOKEN",
+    benefit : "Benefit from smurfs by collecting blueberries using decentralized application.",
+    whitepaper : "Whitepaper",
+    presale : "Pre-Sale",
+    tgch : "Telegram Channel",
+    tgen : "Telegram English",
+    tgchi : "Telegram Chinese",
+    ourMission : "OUR MISSION",
+    aboutST : "The project smurfs token started in second quarter of 2023 , the project is made up of multiple aspects. first, to create a crypto token by the name of smurfs token. second,to create an API that will increase functionality for developers,so they can integrate it with their systems and make the payment process simple. third,to develop a B2E game by the name blueberry through collecting blueberries.lastly,NFT market to buy and sell smurfs nft and develop it",
+    sam: "Sam Freude",
+    saminfo: "CEO 4 years of experience in developing and managing crypto currency projects.",
+    sarah : "Sandra",
+    sarahinfo : "marketing director with experience in marketing.",
+    blueberry : "BLUEBERRY GAME",
+    nftb2e : "NFT smurfs was submitted inside a game with smurfs having different personalities and abilities.who own any of it will get daily return from blueberries without doing any effort but according to smurf ability and specifications and it will be source of earning money. all what you have to do is to buy NFT and you will watch the smurf inside the game collecting it by itself.when BB reach a limit automatically it will convert to SST and deposit in your wallet .The application will be available in appstore and playstore.",
+    readmore : "mining hours are limited and it increases with developing it by buying accessories as hats,basket,shoes..we have six smurfs:",
+    papasm:"1- papa smurf the special character.",
+    smurfette :"2- smurfette the most skilled one in collecting blueberries.",
+    qsm :"3- quick smurf is the fastest between the others.",
+    stsm:"4- strong smurf has stamina.",
+    jksm :"5- smurf joking loves to have fun with smurfs.",
+    clsm:"6- clumsy smurf tripping over anything.",
+    readmoreBtn : "Read more..",
+    appstore : "App Store",
+    playstore : "Play Store",
+    smmarket :"Smurfs market NFT",
+    smmarketin : "A market to buy and sell nft smurfs, with developing it to increase the hours of mining , it will be launched with the game.",
+    limited : "NFT IS A LIMITED NUMBER",
+    papasm1:"papa smurf the special character.",
+    smurfette2 :"smurfette the most skilled one in collecting blueberries.",
+    qsm3 :"quick smurf is the fastest between the others.",
+    stsm4:"strong smurf has stamina.",
+    jksm5 :"smurf joking loves to have fun with smurfs.",
+    clsm6:"clumsy smurf tripping over anything.",
+    tokenmic:"TOKENOMICS",
+    renounce :"Renounce Ownership after Launch",
+    maxs:"Max supply ",
+    totals:"Total supply ",
+    pres:"Presale ",
+    liq:"Liquidity ",
+    mar:"Marketing ",
+    cex:"Cex ",
+    gamenft:"Game ",
+    team:"Team ",
+    roadm:"Roadmap",
+    Q1:"Q3 2023",
+    Buil:"Built our team",
+    Weblaunch:"Website launch",
+    whitepaperR:"Whitepaper release",
+    Q2:"Q4 2023",
+    Gov:"Audit contract and KYC",
+    ido:"IDO",
+    marketing:"Marketing campaign",
+    CMC:"CMC",
+    CG:"CG",
+    Q3:"Q1 2024",
+    gameR:"Game release",
+    nftR:"Releasing NFT market smurfs",
+    mark10:"Marketing every 10 days for 4 months",
+    Q4:"Q2 2024",
+    apiLaunch:"API launch",
+    listing:"Listing major cex",
+    Q5:"Q3 2024",
+    roadmapv2:"Road map V2",
+    partners:"Partners",
+    needh:"Need Help ? ",
+    contactUs:"Contact Us",
+    email:"support@smurfsst.com",
+    sendM:"Send Message",
+    cu:"CONTACT US",
+    Email:"Email : ",
+    by:"By",
+    copyr:"Copyright © 2023. All Rights Reserved",
+    st:"SMURFS TOKEN",
+    tax:"Tax",
+    b:"5% Buy",
+    s:"5% Sell",
+    m:"2% Marketing",
+    l:"1% Listing cex",
+    d:"2% Development",
+    ree:"Read the development agreement",
+    sixity:"60% from NFT sellings will be burned and 30% will be from listing cex.",
+    mark11:"Continue marketing campaign",
+    newS1:"- It is a short roadmap and easy to implement because we deal with the global hyperlink infosystem which allows for rapid development and ensuring high security.",
+    newS2:"- The game will be released 4 months after the launch of the project and the marketing campaign will start two months before the release of the game and two months after it, and burning will start when mining begins, this ensures stability and strength of SST"
+    ,liqPool:"1% To the liquidity pool",
+    tokenHolders:"1% To all token holders",
+    Burn:"Burn SST",
+    partnership:"Partnership",
+    vesting:"-Tokens marketing 100M vesting 8% every 10 days to ensure strong marketing word and deed for 4 months.",
+    unlock_1:"Unlock time 1/12/2023",
+    tokenmining:"-Tokens game 200M for mining not for supply.",
+    unlock_2:"Unlock time 1/2/2024",
+    teamTo:"-Team token 30M vesting 20% every 10  days.",
+    unlock_3:"Unlock time 1/3/2024",
+    platformToken:"-Tokens platforms 150M not for supply.",
+    unlock_4:"Unlock time 1/5/2024",
+    cesar : "Moderator and Marketing Officer in China",
+    mark_smith:"the developer that is responsible to communicate with hyperlink infosystem company"
+  }
+  ,
+  ch: {
+    home: "首页",
+    about: "关于",
+    game: "游戏",
+    NFT: 'NFT',
+    Token: '代币',
+    Roadmap: '路线图',
+    contact: "联系我们",
+    selectLanguage: "English",
+    twitter: "推特",
+    smurfs: "蓝精灵",
+    token: "代币",
+    benefit: "通过使用去中心化应用程序收集蓝莓来从蓝精灵中获益。",
+    whitepaper: "白皮书",
+    presale: "预售",
+    tgch: "Telegram 频道",
+    tgen: "Telegram 英文",
+    tgchi: "Telegram 中文",
+    ourMission: "我们的使命",
+    aboutST: "蓝精灵代币项目始于2023年第二季度，该项目包括多个方面。首先，创建一个名为蓝精灵代币的加密代币。其次，创建一个API，以增加开发人员的功能，使他们可以将其与其系统集成并简化付款流程。第三，通过收集蓝莓来开发一个名为蓝莓的B2E游戏。最后，创建一个NFT市场，用于购买和出售蓝精灵NFT并进行开发。",
+    sam: "Sam Freude",
+    saminfo: "首席执行官，拥有4年开发和管理加密货币项目的经验。",
+    sarah: "Sandra",
+    sarahinfo: "市场总监，具有市场营销经验。",
+    blueberry: "蓝莓游戏",
+    nftb2e: "NFT蓝精灵被提交到了一个带有不同性格和能力的蓝精灵游戏中。拥有任何NFT的人将每天根据蓝精灵的能力和规格获得蓝莓的回报，无需付出任何努力，它将成为赚钱的源泉。你所需要做的就是购买NFT，然后你将看到蓝精灵在游戏中自动收集它。当蓝莓达到一定限额时，它将自动转换为SST并存入您的钱包。该应用程序将在App Store和Play Store上提供。",
+    readmore: "挖矿时间有限，通过购买配件如帽子、篮子、鞋子来增加挖矿时间。我们有六种蓝精灵：",
+    papasm: "1- 爸爸蓝精灵，特殊角色。",
+    smurfette: "2- 斯黛拉蓝精灵，最擅长收集蓝莓。",
+    qsm: "3- 快速蓝精灵，速度最快。",
+    stsm: "4- 强壮蓝精灵，具有耐力。",
+    jksm: "5- 开玩笑蓝精灵，喜欢和蓝精灵一起玩乐。",
+    clsm: "6- 笨拙蓝精灵，总是绊倒在任何东西上。",
+    readmoreBtn: "阅读更多..",
+    appstore: "App Store",
+    playstore: "Play Store",
+    smmarket: "蓝精灵NFT市场",
+    smmarketin: "一个购买和出售NFT蓝精灵的市场，随着游戏的发展将提高挖矿时间，将与游戏一起推出。",
+    limited: "NFT数量有限",
+    papasm1: "1- 爸爸蓝精灵，特殊角色。",
+    smurfette2: "2- 斯黛拉蓝精灵，最擅长收集蓝莓。",
+    qsm3: "3- 快速蓝精灵，速度最快。",
+    stsm4: "4- 强壮蓝精灵，具有耐力。",
+    jksm5: "5- 开玩笑蓝精灵，喜欢和蓝精灵一起玩乐。",
+    clsm6: "6- 笨拙蓝精灵，总是绊倒在任何东西上。",
+    tokenmic: "代币经济",
+    renounce: "启动后放弃所有权",
+    maxs: "最大供应量",
+    totals: "总供应量",
+    pres: "预售",
+    liq: "流动性",
+    mar: "市场营销",
+    cex: "中心化交易所",
+    gamenft: "游戏",
+    team: "团队",
+    roadm: "路线图",
+    Q1: "2023年第三季度",
+    Buil: "建立我们的团队",
+    Weblaunch: "网站发布",
+    whitepaperR: "发布白皮书",
+    Q2: "2023年第四季度",
+    Gov: "审计合同和KYC",
+    ido: "IDO",
+    marketing: "市场营销活动",
+    CMC: "CMC",
+    CG: "CG",
+    Q3: "2024年第一季度",
+    gameR: "发布游戏",
+    nftR: "发布NFT蓝精灵市场",
+    mark10: "每10天进行一次市场营销活动，为期4个月",
+    Q4: "2024年第二季度",
+    apiLaunch: "API发布",
+    listing: "在主要中心化交易所上市",
+    Q5: "2024年第三季度",
+    roadmapv2: "路线图版本2",
+    partners: "合作伙伴",
+    needh: "需要帮助吗？",
+    contactUs: "联系我们",
+    email: "support@smurfsst.com",
+    sendM: "发送消息",
+    cu: "联系我们",
+    Email: "邮箱：",
+    by: "由",
+    copyr: "版权所有 © 2023。保留所有权利",
+    st: "蓝精灵代币",
+    tax: "税",
+    b: "购买5%",
+    s: "出售5%",
+    m: "场营销2%",
+    l: "中心化交易所上市1%",
+    d: "开发2%",
+    ree: "阅读开发协议",
+    sixity: "来自NFT销售的60%将被销毁，30%将来自中心化交易所的上市。",
+    mark11: "继续市场营销活动",
+    newS1: "- 这是一个简短且易于实施的路线图，因为我们与全球超链接信息系统合作，可以快速开发并确保高度安全性。",
+    newS2: "- 游戏将在项目启动后的4个月内发布，市场营销活动将在游戏发布前两个月和之后的两个月开始，燃烧将在挖矿开始时启动，这确保了SST的稳定性和强度。"
+    , liqPool: "1%用于流动性池",
+    tokenHolders: "1%分给所有代币持有者",
+    Burn: "销毁SST",
+    partnership: "合作伙伴关系",
+    vesting: "-代币市场100M，每10天解锁8%，以确保强大的市场推广，持续4个月。",
+    unlock_1: "解锁时间：2023年1月12日",
+    tokenmining: "-代币游戏200M用于挖矿，不供应。",
+    unlock_2: "解锁时间：2024年2月1日",
+    teamTo: "-团队代币30M，每10天解锁20%。",
+    unlock_3: "解锁时间：2024年3月1日",
+    platformToken: "-平台代币150M，不供应。",
+    unlock_4: "解锁时间：2024年5月1日",
+    cesar :"中国的版主兼营销官",
+    mark_smith:"负责与 Hyperlink Infosystem 公司沟通的开发人员"
+
+  }
+  
+};
+
+const languageButton = document.getElementById("languageButton");
+languageButton.addEventListener("click", () => {
+  const currentLanguage = localStorage.getItem("lang") || "en";
+  const newLanguage = currentLanguage === "en" ? "ch" : "en";
+  setLanguage(newLanguage);
+  localStorage.setItem("lang", newLanguage);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en";
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+};
+
+
+
+
+
+
+
+function changePlaceholderLanguage() {
+  var placeholderName = document.getElementById("name");
+  var placeholderEmail = document.getElementById("email");
+  var placeholderSubject = document.getElementById("subject");
+  var placeholderMessage = document.getElementById("message");
+  var x = localStorage.getItem('lang');
+
+
+  if (x === 'en') {
+    placeholderName.placeholder = "Your Name";
+    placeholderEmail.placeholder = "Your Email";
+    placeholderSubject.placeholder = "Subject";
+    placeholderMessage.placeholder = "Message";
+  } else {
+    placeholderName.placeholder = "你的名字";
+    placeholderEmail.placeholder = "你的邮件";
+    placeholderSubject.placeholder = "主题";
+    placeholderMessage.placeholder = "信息";
+  }
+
+}
+languageButton.addEventListener("change", changePlaceholderLanguage);
+
+window.addEventListener('DOMContentLoaded', changePlaceholderLanguage);
+
+
+
